1a7b588805aa71de030eaaab3e382b8ae8aff3c5
Brijendra Nag <brijendra98@gmail.com>
2017-09-16 18:26:03 -0400

    fixing issues

--- bnag_code/get_prices.js
+++ bnag_code/get_prices.js
@@ -1,7 +1,8 @@
 var request = require("request");
-var get_price = function(crypto_currency, user_currency) {
 
-var options = { method: 'GET',
+function getPrice(crypto_currency, user_currency, session) {
+var ret = -1;
+var options = {method: 'GET',
   url: `https://api.coinbase.com/v2/prices/${crypto_currency}-USD/buy`,
 };
 request(options, function (error, response, body) {
@@ -14,13 +15,16 @@ request(options, function (error, response, body) {
      { authorization: 'Basic aGFja3RoZW5vcnRoOTE3OTI3MTMyOmsyNGM5aHFqaW5jdThmZGxtOWdxZjVpNzJr' } };
   request(options, function (error, response, body) {
     if (error) throw new Error(error);
+    //console.log(body);
     body = JSON.parse(body);
-    //console.log(`1 ${crypto_currency} = ${body.amount} ${body.to[0].quotecurrency}`);
     session.send(`1 ${crypto_currency} = ${body.amount} ${body.to[0].quotecurrency}`);
     // Use body to do whatever stuff (return from function or send to user etc...). I'm just logging it for now.
   });
 });
 }
-
 // Example:get_price('ETH', 'GBP');
 // Example: get_price('LTC', 'INR');
+
+module.exports = {
+  getPriceFunc: getPrice
+};
