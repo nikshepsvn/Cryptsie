d0be28f1945d61c94186a484997fbaa60abd477f
nikshepsvn <nikshepsvn@gmail.com>
2017-09-16 17:50:32 -0400

    testing coinbase OAuth

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ A simple echo bot for the Microsoft Bot Framework.
 
 var restify = require('restify');
 var builder = require('botbuilder');
+var firebase = require("firebase");
+var config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+};
+var coinbase = require('/coinbase.js');
+firebase.initializeApp(config);
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -30,5 +39,19 @@ server.post('/api/messages', connector.listen());
 
 // Create your bot with a function to receive messages from the user
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said(AY): %s", session.message.text);
+    //session.send("You said(AY): %s", session.message.text);
+    var msg = session.message.text;
+    if(msg == "a"){
+        coinbase.requestCoinbaseOAuthAccess(session);
+    } else if(msg == "b"){
+    	session.send(test2(msg));
+    }
 });
+
+function test(msg){
+	return "Brijendar";
+}
+
+function test2(msg) {
+	return "Garodia";
+}
