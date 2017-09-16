38d09fc25e54c0737d5770f909fec533c07a6af2
AakarshM <a3madhav@edu.uwaterloo.ca>
2017-09-16 16:16:07 -0400

    firebase config

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ A simple echo bot for the Microsoft Bot Framework.
 var restify = require('restify');
 var builder = require('botbuilder');
 var firebase = require("firebase");
-var config = {
+
+//Setup Firebase
+var firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 };
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
+var defaultDatabase = defaultApp.database(); //Initialize firebase database
+
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -48,7 +52,7 @@ var bot = new builder.UniversalBot(connector, function (session) {
 });
 
 function test(msg){
-	return "Brijendar";
+	return firebase.app.name();
 }
 
 function test2(msg) {
