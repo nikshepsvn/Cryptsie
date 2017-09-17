75752db82cb5b8b7efa35b7a6321b6be97129845
Brijendra Nag <brijendra98@gmail.com>
2017-09-17 06:01:31 -0400

    changed back news to working state

--- app.js
+++ app.js
@@ -58,19 +58,7 @@ var bot = new builder.UniversalBot(connector, function (session) {
             });
           });
     }
-    else if (msg == "news") {
-
-      return new builder.ThumbnailCard(session)
-        .title('BotFramework Thumbnail Card')
-        .subtitle('Your bots — wherever your users are talking')
-        .text('Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.')
-        .images([
-            builder.CardImage.create(session, 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg')
-        ])
-        .buttons([
-            builder.CardAction.openUrl(session, 'https://docs.microsoft.com/bot-framework', 'Get Started')
-        ]);
-        
+    else if (msg == "news") {        
       var holdings = ["Bitcoin", "Ethereum", "Litecoin"];
       _.each(holdings, function(holding) {
         news.getNewsFunc(holding, 3, function(news_data){
