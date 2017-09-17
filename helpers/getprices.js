2f9d3a3da0265db97ac8a9f3f394c1a02d7c5e2f
AakarshM <a3madhav@edu.uwaterloo.ca>
2017-09-17 08:29:24 -0400

    currency

--- helpers/getprices.js
+++ helpers/getprices.js
@@ -41,15 +41,15 @@ request(options, function (error, response, body) {
   if (error) throw new Error(error);
   body = JSON.parse(body);
   var options = { method: 'GET',
-    url: 'https://xecdapi.xe.com/v1/convert_from/',
-    qs: { to: user_currency, from: 'usd', amount: body.data.amount },
+    url: 'https://xecdapi.xe.com/v1/convert_to/',
+    qs: { to: 'usd', from: user_currency, amount: body.data.amount },
     headers:
      { authorization: 'Basic aGFja3RoZW5vcnRoOTE3OTI3MTMyOmsyNGM5aHFqaW5jdThmZGxtOWdxZjVpNzJr' } };
   request(options, function (error, response, body) {
     if (error) throw new Error(error);
     //console.log(body);
     body = JSON.parse(body);
-    session.send(`1 ${crypto_currency} = ${body.from[0].mid} ${body.to[0].quotecurrency}`);
+    session.send(`1 ${crypto_currency} = ${body.from[0].mid} ${body.from[0].quotecurrency}`);
     // Use body to do whatever stuff (return from function or send to user etc...). I'm just logging it for now.
   });
 });
