6b6825264a6bf6b43496278624f29cbec428e96f
AakarshM <a3madhav@edu.uwaterloo.ca>
2017-09-17 05:46:09 -0400

    cbase

--- app.js
+++ app.js
@@ -382,6 +382,10 @@ request(options, function (error, response, body) {
     body = JSON.parse(body);
     session.send(`1 ${crypto_currency} = ${body.amount} ${body.to[0].quotecurrency}`);
     // Use body to do whatever stuff (return from function or send to user etc...). I'm just logging it for now.
+    dbRef.update({
+      crypto_currency: body.amount
+    });
+
   });
 });
 }
